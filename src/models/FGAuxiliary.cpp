6ee30cc1865839127d1202101e1d91295dd04080
ewan19 <boogewan@hotmail.com>
2024-02-07 12:03:33 +0100

    test j'essaye

--- src/models/FGAuxiliary.cpp
+++ src/models/FGAuxiliary.cpp
@@ -681,7 +681,7 @@ void FGAuxiliary::loadgrid()
   in.close();
 }
 
-
+// on test
 // Ici on rentre dans la boite bigflow à la position initiale : ( 1000 [m] (altitude), 0 [m] (Longueur), 4000 [m] (largeur) )
 // Donc je suis en face de la boite au milieu 
 //
