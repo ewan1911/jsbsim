d0ce8c85065253008e2d7397e5709f812bb099db
Simon Diard <simon.diard@gmail.com>
2024-02-07 18:39:31 +0100

    test simon

--- src/models/FGAuxiliary.cpp
+++ src/models/FGAuxiliary.cpp
@@ -682,7 +682,7 @@ void FGAuxiliary::loadgrid()
 }
 
 // on test
-// re test encore
+// re test encore 
 // Ici on rentre dans la boite bigflow à la position initiale : ( 1000 [m] (altitude), 0 [m] (Longueur), 4000 [m] (largeur) )
 // Donc je suis en face de la boite au milieu 
 //
